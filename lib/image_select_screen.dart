2f8db3ec92433edef66b827b312ff05f78adec0a
bz0 <bzz0217@yahoo.co.jp>
2025-10-13 17:19:53 +0900

    画像選択

--- lib/image_select_screen.dart
+++ lib/image_select_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'l10n/app_localizations.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as image_lib;
+import 'package:flutter/foundation.dart';
 
 class ImageSelectScreen extends StatefulWidget {
   const ImageSelectScreen({super.key});
@@ -9,9 +12,35 @@ class ImageSelectScreen extends StatefulWidget {
 }
 
 class _ImageSelectScreenState extends State<ImageSelectScreen> {
+  final ImagePicker _picker = ImagePicker();
+  Uint8List? _imageBitmap;
+
+  Future<void> _selectImage() async {
+    final XFile? imageFile = await _picker.pickImage(source: ImageSource.gallery);
+    final imageBitmap = await imageFile?.readAsBytes();
+    assert(imageBitmap != null);
+    if (imageBitmap == null) return;
+
+    final image = image_lib.decodeImage(imageBitmap);
+    assert(image != null);
+    if (image == null) return;
+
+    final image_lib.Image resizedImage;
+    if (image.width > image.height) {
+      resizedImage = image_lib.copyResize(image, width: 500);
+    } else {
+      resizedImage = image_lib.copyResize(image, height: 500);
+    }
+
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(resizedImage);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context);
+    final imageBitmap = _imageBitmap;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -22,7 +51,7 @@ class _ImageSelectScreenState extends State<ImageSelectScreen> {
           children: [
             ElevatedButton(
               child: Text(l10n?.imageSelect ?? '画像を選択'),
-              onPressed: () {},
+              onPressed: () => _selectImage(),
             ),
             ElevatedButton(
               child: Text(l10n?.imageEdit ?? '画像を編集'),
