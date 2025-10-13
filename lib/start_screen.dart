ff906832cc2bd5539d1720ffcb113d3effafd9ab
bz0 <bzz0217@yahoo.co.jp>
2025-10-13 13:57:22 +0900

    スタート画面作成

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -6,8 +7,24 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Edit Snap'),
+      ),
       body: Center(
-        child: Text('Start Screen'),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                Text(
+                    'こんにちは!\n今日は${DateFormat('yyyy年MM月dd日 HH:mm').format(DateTime.now())}です',
+                    textAlign: TextAlign.center, 
+                ),
+                ElevatedButton(
+                    child: const Text('開始する'),
+                    onPressed: () {},
+                ),
+            ],
+        ),
       ),
     );
   }
